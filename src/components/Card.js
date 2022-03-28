9bd09d92cfa62fc4ab9339b4bd8c584d9b5a372d
Vladimir Fedarenka <vfedarenka074@gmail.com>
2022-03-28 21:57:00 +0200

    add drag drop listener

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,13 @@ const Card = () => {
         <div>
             <h1>card</h1>
             {cardList.map(card => 
-                <div className={'Card'}>
+                <div 
+                onDragEnd={(e) => console.log(card.text)}
+                onDragLeave={(e) => console.log(card.text)}
+                onDragOver={(e) => console.log(card.text)}
+                onDragStart={(e) => console.log(card.text)}
+                draggable={true}
+                className={'Card'} >
                     {card.text}
                 </div>
             )}
